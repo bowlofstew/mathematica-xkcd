614ffde183cde6f2c5767d605170e1618a62f91b
Stewart Henderson <shenderson@mozilla.com>
2020-04-14 20:46:59 -0500

    Added PacletInfo file

---
+++ PacletInfo.m
@@ -0,0 +1,13 @@
+Paclet[
+    Name -> "xkcd",
+    Version -> "2100.0",
+    MathematicaVersion -> "10.1+",
+    Root -> ".",
+    Internal -> True,
+    Extensions -> 
+        {
+            {"Kernel", Root -> ".", Context -> "xkcd`"},
+            {"Documentation", Language -> "English"},
+      {"LibraryLink"}
+        }
+]
